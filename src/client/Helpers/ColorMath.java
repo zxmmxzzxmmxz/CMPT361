bc78ad4e4babb0e96c39419350b056438306031a
ximinz <ximinz@sfu.ca>
2017-10-06 13:29:03 -0700

    WIP

---
+++ src/client/Helpers/ColorMath.java
@@ -0,0 +1,9 @@
+package client.Helpers;
+
+import windowing.graphics.Color;
+
+public class ColorMath {
+    public static Color fromDistance(double distance) {
+        return Color.WHITE;
+    }
+}
--- src/line/AntialiasingLineRenderer.java
+++ src/line/AntialiasingLineRenderer.java
@@ -1,10 +1,17 @@
 package line;
 
+import client.Helpers.GraphicMath;
+import geometry.Line;
+import geometry.Point3DH;
 import geometry.Vertex3D;
 import windowing.drawable.Drawable;
+import windowing.graphics.Color;
 import windowing.graphics.Dimensions;
 
 public class AntialiasingLineRenderer implements LineRenderer {
+
+    private static final int PIXEL_RADIUS = 1;
+
     public AntialiasingLineRenderer() {
     }
 
@@ -14,7 +21,17 @@ public class AntialiasingLineRenderer implements LineRenderer {
 
     @Override
     public void drawLine(Vertex3D p1, Vertex3D p2, Drawable panel) {
+        Line line = new Line(p1.getPoint3D(), p2.getPoint3D());
         Dimensions dimensions = panel.getDimensions();
-        
+        for (int y = 0; y <= dimensions.getHeight(); y++) {
+            for (int x = 0; x <= dimensions.getWidth(); x++) {
+                double distance = GraphicMath.distanceFromLine(line, new Point3DH(x, y, 0));
+                if (distance < 2 * PIXEL_RADIUS) {
+                    //then render the point
+                    //otherwise don't
+                    panel.setPixel(x, y, 0, Color.WHITE.asARGB());
+                }
+            }
+        }
     }
 }
